82cf0e3bf614ea798cccf2661c78c9d6f6af9e8b
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-05-09 16:47:58 +0530

    Color picker works now

--- app/assets/javascripts/color_picker.js
+++ app/assets/javascripts/color_picker.js
@@ -1,33 +1,14 @@
-$('.color-picker').spectrum({
+$('.colorpicker').spectrum({
   type: "color",
   showPaletteOnly: "true",
   showInput: "true",
   showInitial: "true"
 });
 
-$('#color-picker-banner-text').spectrum({
+$(".color-picker").spectrum({
   type: "color",
   showPaletteOnly: "true",
   showInput: "true",
-  showInitial: "true"
-});
-
-$("#color-picker-banner-text").spectrum({
-  color: "#f00"
-});
-
-$("#color-picker-banner-link").spectrum({
-  color: "#f00"
-});
-
-$("#color-picker-button").spectrum({
-  color: "#f00"
-});
-
-$("#color-picker-button-text").spectrum({
-  color: "#f00"
-});
-
-$("#color-picker-button-border").spectrum({
-  color: "#f00"
+  showInitial: "true",
+  showPalette: true
 });
