1a9f1a194bded45b3aee8eaaa6c89f76073b415c
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-05-09 09:20:37 +0530

    Pages layout; sample home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < AuthenticatedController
+  layout 'pages' 
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
