68ce7cb8d0908de1f935f78f955249f938de521c
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-05-09 15:49:43 +0530

    Spectrum 2 color picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,4 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
---
+++ app/assets/javascripts/color_picker.js
@@ -0,0 +1,33 @@
+$('.color-picker').spectrum({
+  type: "color",
+  showPaletteOnly: "true",
+  showInput: "true",
+  showInitial: "true"
+});
+
+$('#color-picker-banner-text').spectrum({
+  type: "color",
+  showPaletteOnly: "true",
+  showInput: "true",
+  showInitial: "true"
+});
+
+$("#color-picker-banner-text").spectrum({
+  color: "#f00"
+});
+
+$("#color-picker-banner-link").spectrum({
+  color: "#f00"
+});
+
+$("#color-picker-button").spectrum({
+  color: "#f00"
+});
+
+$("#color-picker-button-text").spectrum({
+  color: "#f00"
+});
+
+$("#color-picker-button-border").spectrum({
+  color: "#f00"
+});
