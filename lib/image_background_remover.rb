74e082dcf5c87d18b9f09e46af70c68a642e5ffa
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-05-10 21:23:24 +0530

    Updated output image name

--- lib/image_background_remover.rb
+++ lib/image_background_remover.rb
@@ -1,6 +1,6 @@
 module ImageBackgroundRemover
   def self.process(image_url)
     result = RemoveBg.from_url(image_url)
-    File.open("test.png", "wb") { |f| f.write(result.data) } 
+    File.open("output.png", "wb") { |f| f.write(result.data) } 
   end
 end
